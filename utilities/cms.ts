44c7ef671c701f70dbfacdea85ecb0a23ec3c1ac
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2023-04-08 06:18:52 +0000

    fixing TS issues in CMS

--- utilities/cms.ts
+++ utilities/cms.ts
@@ -1,5 +1,6 @@
-import PropTypesOriginal from 'prop-types';
+import PropTypesOriginal, { Requireable } from 'prop-types';
 import { EMPTY, empty } from './constants';
+import { ReactElement } from 'react';
 
 /**
  * Used for mapping PropType method names to their corresponding types
@@ -19,34 +20,39 @@ const typeMap = {
   symbol: Symbol,
 };
 
+type GenericObj = { [key: string]: unknown };
+type GenericFn = (...args: unknown[]) => unknown;
+type ValueOf<T> = T[keyof T];
+
 /**
  * Decorates the PropTypes package with actual types so they can be extracted from outside the component
  */
-export const PropTypes = Object.entries(PropTypesOriginal).reduce((PropTypes, [key, val]) => {
+export const PropTypes: typeof PropTypesOriginal = Object.entries(PropTypesOriginal).reduce((PropTypes, [key, val]) => {
   PropTypes[key] = val;
   const assignType = (obj, Type) => {
     obj.type = Type;
     if (obj.isRequired) obj.isRequired.type = Type;
   };
-  if (val.name === 'bound checkType') {
+  const _val = val as (GenericObj | GenericFn);
+  if (_val.name === 'bound checkType') {
     const Type = typeMap[key] ?? null;
     assignType(PropTypes[key], Type);
-  } else if (typeof val === 'function') {
+  } else if (typeof _val === 'function') {
     PropTypes[key] = (Type, ...args) => {
-      const result = val(Type, ...args);
+      const result = _val(Type, ...args);
       assignType(result, Type);
       return result;
     };
   }
   return PropTypes;
-}, {});
+}, { ...PropTypesOriginal });
+
+type RequireableWithType<T> = { type: ValueOf<typeof typeMap> } & Requireable<T>
 
 /**
  * Get just the information needed from one propType
- * @param {PropTypesOriginal.Validator<unknown>} propType - The propType as defined by the 'prop-types' npm package (and decorated by 'utilities/cms')
- * @returns {{ required: Boolean, type: Function }} - The necessary propType info
  */
-export const getPropTypeInfo = propType => ({
+export const getPropTypeInfo = (propType: RequireableWithType<unknown>): { required: Boolean, type: ValueOf<typeof typeMap> } => ({
 
   // if the property exists, then `isRequired` was not defined in the propTypes;
   // therefore, we know this field is required if `isRequired` is missing.
@@ -57,49 +63,40 @@ export const getPropTypeInfo = propType => ({
   type: propType.type,
 });
 
+type Component = { propTypes?: GenericObj, name?: string } & ReactElement<unknown>;
+type PropTuple = [string, RequireableWithType<unknown>];
+
 /**
  * Get props which are available for the CMS to edit
- * @param {React.ReactElement} Component - The React component to get props from
- * @returns {[string, unknown]} - Key/value pairs for each propType for easy iteration
  */
-export const getCMSProps = Component => Object.entries(Component.propTypes ?? EMPTY.OBJ);
-
-/**
- * Component props as { key: value } pairs
- * @typedef {Object.<string, unknown>} Props
- */
-
-/**
- * Gets the default prop values for a given component
- * @param {React.ReactElement} Component - The component which needs default props
- * @returns {Props} - The prop values as key/value pairs
- */
-export const getDefaultProps = Component => {
-  const cmsProps = getCMSProps(Component.propTypes);
-  const defaultPropState = cmsProps.reduce((defaultPropState, [prop, propType]) => {
-    const { required, type } = getPropTypeInfo(propType);
-    if (required) defaultPropState[prop] = empty(type);
-    return defaultPropState;
-  }, empty(Object));
-  return defaultPropState;
+export const getCMSProps = (Component: Component): PropTuple[] => {
+  return Object.entries(Component.propTypes ?? EMPTY.OBJ);
 };
 
 /**
- * An object representing a component as it's used in the page-builder
- * @typedef {Object} CMSComponent
- * @property {string} id
- * @property {React.ReactElement} Component
- * @property {Props} props
- * @property {Array<CMSComponent>} childComponents
+ * Gets the default prop values for a given component
  */
+export const getDefaultProps = (Component: Component): GenericObj => {
+  const cmsProps = getCMSProps(Component);
+  const defaultPropState = cmsProps.reduce((defaultPropState, [prop, propType]: PropTuple) => {
+    const { required, type } = getPropTypeInfo(propType);
+    if (required) defaultPropState[prop] = empty(type);
+    return defaultPropState;
+  }, {});
+  return defaultPropState;
+};
+
+type CMSComponent = {
+  id: string,
+  Component: Component,
+  props: GenericObj,
+  childComponents: CMSComponent[],
+};
 
 /**
  * Creates a new object representing one component with default content
- * @param {React.ReactElement} Component - The actual React component to use
- * @param {number} index - The insertion index in the component array
- * @returns {CMSComponent} - A new component object for use with the CMS
  */
-export const createComponent = (Component, index) => ({
+export const createComponent = (Component: Component, index: number): CMSComponent => ({
   id: `${Component.name}-${Date.now()}-${index}`,
   Component,
   props: getDefaultProps(Component),
