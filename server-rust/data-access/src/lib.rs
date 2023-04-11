b83aeefa63332466e2e0eaf2924ca42a59d54253
lunchspider <lunchspider03@gmail.com>
2023-04-11 21:55:41 +0530

    adds graphql-server crate

--- server-rust/data-access/src/lib.rs
+++ server-rust/data-access/src/lib.rs
@@ -1,3 +1,21 @@
+use mongodb::{
+    options::{ClientOptions, ResolverConfig},
+    Client,
+};
+
 pub mod api;
 pub mod error;
 pub mod model;
+
+pub async fn connect_database() -> Result<Client, error::Error> {
+    let client_uri =
+        std::env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+
+    // A Client is needed to connect to MongoDB:
+    // An extra line of code to work around a DNS issue on Windows:
+    let options =
+        ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
+            .await?;
+    let client = Client::with_options(options)?;
+    Ok(client)
+}
